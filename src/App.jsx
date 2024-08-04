6a9c5f50a6f18d8d1541db959fbf0576db0cb7b9
islamm771 <90573842+islamm771@users.noreply.github.com>
2024-08-04 23:38:11 +0300

    remove full screen button

--- src/App.jsx
+++ src/App.jsx
@@ -2,30 +2,14 @@ import { Route, Routes } from "react-router-dom";
 import { ConfigProvider } from "antd";
 import "./App.css";
 import "../src/styles/css/styles.css";
+import MainPage from "./pages/PagesOutlet/Pages";
 import Chat from "./pages/chat/Chat";
-import { useEffect, useRef } from "react";
+// import AOS from "aos";
+// import "aos/dist/aos.css";
+// AOS.init();
+
 
 function App() {
-  const appRef = useRef(null);
-
-  const enterFullscreen = () => {
-    if (appRef.current.requestFullscreen) {
-      appRef.current.requestFullscreen();
-    } else if (appRef.current.mozRequestFullScreen) { // Firefox
-      appRef.current.mozRequestFullScreen();
-    } else if (appRef.current.webkitRequestFullscreen) { // Chrome, Safari and Opera
-      appRef.current.webkitRequestFullscreen();
-    } else if (appRef.current.msRequestFullscreen) { // IE/Edge
-      appRef.current.msRequestFullscreen();
-    }
-  };
-
-  useEffect(() => {
-    if (screen.width <= 768) {
-      
-    }
-  }, []);
-
   return (
     <ConfigProvider
       theme={{
@@ -45,11 +29,8 @@ function App() {
         },
       }}
     >
-      <div className="App" ref={appRef}>
-        <button className="block md:hidden" id="fullscreenButton" onClick={enterFullscreen}>
-          Enter Fullscreen
-        </button>
-        <Chat />
+      <div className="App">
+          <Chat />
         {/* <Routes>
           <Route path="/" element={<Chat />} />
         </Routes> */}
